649560321f99cc5d214e3bee33ff315ee8fd4b6f
Denis Kucevic <denis.kucevic@advania.is>
2022-04-27 23:35:31 +0200

    restrucuring layouts

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -6,30 +6,32 @@ import { Header } from './layouts/header';
 import './styles/photon.min.css';
 
 const Hello = () => {
+  return <div className="window">HELLO PAGE</div>;
+};
+
+export default function App() {
   return (
-    <div className="window">
+    <div>
       <Header>
         <BclTitle>This is a Header</BclTitle>
       </Header>
-      <div className="window-content">
-        <div className="pane-group">
-          <div className="pane-sm sidebar">...</div>
-          <div className="pane">...</div>
+      <div className="window">
+        <div className="window-content">
+          <div className="pane-group">
+            <nav className="pane-sm sidebar">...</nav>
+            <main className="pane">
+              <Router>
+                <Routes>
+                  <Route path="/" element={<Hello />} />
+                </Routes>
+              </Router>
+            </main>
+          </div>
         </div>
       </div>
       <Footer>
         <BclTitle>Developed with ❤️ by Brixi & JonnXor!</BclTitle>
       </Footer>
     </div>
-  );
-};
-
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
   );
 }
